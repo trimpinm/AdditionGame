1fcb800a636d7d4840f434f9b85a53c41825c1b2
Maggie Trimpin <trimpinm@gmail.com>
2024-02-26 15:54:53 -0500

    Initial Commit

---
+++ Addition Game/Addition_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Addition_GameApp.swift
+//  Addition Game
+//
+//  Created by Maggie Trimpin on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Addition_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
